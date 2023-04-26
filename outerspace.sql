9916b4f8846c5c8aebd95cecffabf1aa982eecca
Tanya Shylock <tourtanya@gmail.com>
2023-04-25 20:15:17 -0600

    update outerspace

--- outerspace.sql
+++ outerspace.sql
@@ -1,34 +1,34 @@
 CREATE TABLE stars (
     id TEXT PRIMARY KEY,
-    temp Text
+    temp TEXT
 );
 
 CREATE TABLE planets (
     id TEXT PRIMARY KEY,
     orbital_yrs FLOAT NOT NULL,
-    star_name TEXT REFERENCES stars
+    star_id TEXT REFERENCES stars
 );
 
 CREATE TABLE moons (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    planet_name TEXT REFERENCES planets
+    planet_id TEXT REFERENCES planets
 );
 
 INSERT INTO stars (id,temp)
 VALUES
-('The Sun' ,'5800°K'),
+('The Sun', '5800°K'),
 ('Proxima Centauri', '3042°K'),
-('Gliese 876','3192°K');
+('Gliese 876', '3192°K');
 
-INSERT INTO planets (id,orbital_yrs)
+INSERT INTO planets (id, orbital_yrs, star_id)
 VALUES
-('Earth',1.00),
-('Mars',1.882),
-('Venus',0.62),
-('Proxima Centauri b',0.03),
-('Gliese 876 b',0.236);
+('Earth', 1.00, 'The Sun'),
+('Mars', 1.882, 'The Sun'),
+('Venus', 0.62, 'The Sun'),
+('Proxima Centauri b', 0.03, 'Proxima Centauri'),
+('Gliese 876 b',0.236, 'Gliese 876');
 
-INSERT INTO moons (name)
+INSERT INTO moons (name, planet_id)
 VALUES
-('The Moon'),('Phobos'),('Deimos');
+('The Moon', 'Earth'),('Phobos', 'Mars'),('Deimos', 'Mars');
